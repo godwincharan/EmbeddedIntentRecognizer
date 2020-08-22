e9e0925ccdb16370f28a509d5b2109ac81b3b489
Charan DSouza <godwincharan@gmail.com>
2020-08-22 16:58:28 +0200

    Adding all the states to state manager
    Issue: #4

--- lib/state_machine/include/state.h
+++ lib/state_machine/include/state.h
@@ -9,19 +9,28 @@ namespace state_machine
 class State
 {
 public:
+    using SPtr = std::shared_ptr<State>;
+    struct StateCompare
+    {
+        bool operator() (const State::SPtr& lhs, const State::SPtr& rhs) const
+        {
+            return *lhs < *rhs;
+        }
+    };
+
     explicit State(const std::string& state_str) noexcept;
     virtual ~State();
 
     const std::string& GetState() const noexcept;
-    void AddNextState(const State& state) noexcept;
-    bool HasNextState(const State& state) const noexcept;
+    void AddNextState(const State::SPtr state) noexcept;
+    bool HasNextState(const State::SPtr state) const noexcept;
 
     bool operator< (const State &state) const noexcept;
 
     virtual bool IsValid() const noexcept;
 private:
     std::string state_str_;
-    std::set<State> next_states_;
+    std::set<State::SPtr, StateCompare> next_states_;
 };
 
 class InitialState final : public State
--- test/state_machine/state_manager_test.cpp
+++ test/state_machine/state_manager_test.cpp
@@ -16,3 +16,19 @@ TEST(StateManagerTest, CheckInitialState)
 
     EXPECT_TRUE(state_manager->GetCurrentState()->IsValid());
 }
+
+TEST(StateManagerTest, CreateState)
+{
+    using namespace state_machine;
+    auto state_manager{std::make_unique<StateManager>()};
+    EXPECT_NE(state_manager, nullptr);
+
+    EXPECT_TRUE(state_manager->AddState("State"));
+    EXPECT_FALSE(state_manager->AddState("State"));
+
+    auto state{std::make_shared<State>("State")};
+    EXPECT_TRUE(state_manager->HasState(state));
+
+    auto no_state{std::make_shared<State>("No State")};
+    EXPECT_FALSE(state_manager->HasState(no_state));
+}
