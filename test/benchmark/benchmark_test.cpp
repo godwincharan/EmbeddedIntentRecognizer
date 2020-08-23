cd950b83cddae57dc81a23b184d967715e8eacf5
Charan DSouza <godwincharan@gmail.com>
2020-08-23 16:16:32 +0200

    Added Benchmark test
    Added script file to build and run the tests
    
    Issue: #19

---
+++ test/benchmark/benchmark_test.cpp
@@ -0,0 +1,33 @@
+#include <benchmark/benchmark.h>
+#include <state_machine_factory.h>
+
+class StateMachineGeneratorBenchmark : public ::benchmark::Fixture
+{
+public:
+    StateMachineGeneratorBenchmark()
+    {
+        Repetitions(20);
+        ReportAggregatesOnly(false);
+        Unit(benchmark::kMicrosecond);
+    }
+
+    void RunBenchmark(benchmark::State& state)
+    {
+        for(auto _ : state)
+        {
+            using namespace state_machine;
+            std::string csv_file_name{"data/weather_intent_data.csv"};
+            auto state_manager = StateMachineFactory::Get().CreateStateManagerFor(csv_file_name);
+            benchmark::ClobberMemory();
+            state.counters["count"] = 1;
+            state.counters["Rate"] = ::benchmark::Counter(1,benchmark::Counter::kIsRate);
+        }
+    }
+};
+
+BENCHMARK_F(StateMachineGeneratorBenchmark, CsvFile)(benchmark::State& state)
+{
+    RunBenchmark(state);
+}
+
+BENCHMARK_MAIN();
